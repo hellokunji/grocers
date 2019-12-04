a5630964685d8f63b8994bdd3691872c46251247
kunjimeena <hellokunjimeena@gmail.com>
2019-12-05 00:39:38 +0530

    updates

--- server/models/index.js
+++ server/models/index.js
@@ -6,7 +6,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config.json')[env];
-const db = {};
+let db = {};
 var pg = require('pg');
 pg.defaults.ssl = true;
 let sequelize;
